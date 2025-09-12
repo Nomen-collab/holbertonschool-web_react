bdbe06e2fa21bf014f455ca35ff732c691734914
Nomen-collab <nomenaratsimba01@gmail.com>
2025-09-13 00:04:04 +0200

    task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -48,4 +48,38 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
 
 /* Exemple d’utilisation */
 console.log(printTeacher("John", "Doe")); // J. Doe
+/* Interface du constructeur */
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+/* Interface de la classe */
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+/* Implémentation de la classe */
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+/* Exemple d’utilisation */
+const student = new StudentClass("John", "Doe");
+console.log(student.displayName());   // John
+console.log(student.workOnHomework()); // Currently working
 
